1d25a82699f1b685eb8cb12d0f109ab3c709087b
John <polatsultonov97@gmail.com>
2026-09-11 06:06:06 +0900

    feat: learn boolean in python

--- primitive.py
+++ primitive.py
@@ -28,3 +28,20 @@ print(f"the result(3): {result}")
 
 
 print("=== Boolean===")
+
+# functions > type() input() bool() int() str()
+y = input("Give your value for y: ")
+print("y: ", y)
+
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+# TRUTHY vs FALSY value
+# TRUTHY: True100 -100 "MIT"
+# FALSY: False 0 "" None
+
+test_falsy = "" or False or None or 0
+print("test_falsy:",  bool(test_falsy))
+
+test_truthy = "MIT"
+print("test_truthy:",  bool(test_truthy))
