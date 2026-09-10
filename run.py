89096489e94ecb0047fdfb00ed9f90cb60ec0ed6
John <polatsultonov97@gmail.com>
2026-09-11 05:24:12 +0900

    Dunderlar haqida

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
-a = 147
-print("a:", a)
-
-message = "Hello World!"
+# Dunder __builtins__, __init__
+message = "Python: Everything is object!"
 print(message)
 
+result = type(message)
+print("result:", result)
 
-#okay this is a test line!
-#I set up the git push command!
+'''
+In Python, there are builtin tools:
+(1) TYPES > int float str list dict
+(2) FUNCTION > print() len() input() type() str() int()
+(3) CONSTANT > True False None
+'''
+
+print(dir(__builtins__))
